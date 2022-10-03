507f50e5916028c46a1b0bfde5b650e465ca709d
dbuendia <buendia.barcelo@gmail.com>
2022-10-03 15:31:08 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./Header";
+import Employees from "./Employees";
+import Footer from "./Footer";
 
 function App() {
+  // Accedemos al almacenamiento local para setear el valor que hubiera sido seleccionado para selectedTeam.
+  // Si no hay ninguno, por defecto pondremos "TeamA"
+  const [selectedTeam, setTeam] = useState(
+    JSON.parse(localStorage.getItem("selectedTeam")) || "TeamA"
+  );
+
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: "Bob Jones",
+      designation: "JavaScript Developer",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 2,
+      fullName: "Jill Bailey",
+      designation: "Node Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 3,
+      fullName: "Gail Shepherd",
+      designation: "Java Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 4,
+      fullName: "Sam Reynolds",
+      designation: "React Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB",
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC",
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+  ]);
+
+  // Cada vez que cambie el array de empleados guardaremos en el localStorage la lista de empleados y el selectedTeamj
+  useEffect(() => {
+    localStorage.setItem("employeeList", JSON.stringify(employees));
+  }, [employees]);
+
+  useEffect(() => {
+    localStorage.setItem("selectedTeam", JSON.stringify(selectedTeam));
+  }, [selectedTeam]);
+
+  function handleTeamSelectionChange(e) {
+    setTeam(e.target.value);
+  }
+
+  function handleEmployeeCardClick(e) {
+    // Creo un nuevo array de empleados y recorremos el array vigente de empleados con .map
+    const transformedEmployees = employees.map((employee) =>
+      // Si el id del empleado en el array de empleados coincide con el id del empleado de la card seleccionada...
+      // (Leemos el currentTarget (el elemento que tiene el evento attached) y lo parseamos a int porque en realidad es un string)
+      employee.id === parseInt(e.currentTarget.id)
+        ? // ... Y si el empleado seleccionado ya está dentro del equipo seleccionado en el dropdown
+          employee.teamName === selectedTeam
+          ? // Eliminamos a ese empleado de ese equipo borrando su teamName
+            { ...employee, teamName: "" }
+          : // O lo agregamos al equipo seleccionado en el dropdown añadiendo ese valor a su teamName
+            { ...employee, teamName: selectedTeam }
+        : employee
+    );
+    // Seteamos el nuevo estado de empleados
+    setEmployees(transformedEmployees);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        selectedTeam={selectedTeam}
+        // Que nos devuelva un array con los empleados que estén en el team seleccionado y contamos su length para obtener el número
+        teamMemberCount={
+          employees.filter((employee) => {
+            return employee.teamName === selectedTeam;
+          }).length
+        }
+      />
+      <Employees
+        employees={employees}
+        selectedTeam={selectedTeam}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+      />
+      <Footer />
     </div>
   );
 }
