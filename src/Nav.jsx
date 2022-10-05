bb6ec92cc5a1944ee082bed1bdccb85ca48614f1
dbuendia <buendia.barcelo@gmail.com>
2022-10-05 16:24:14 +0200

    Fixed routes

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link className="nav-link" to="/team-member-allocation">
             Home
           </Link>
         </li>
